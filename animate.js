906922a95adf122f7e21798c02421a84ca041c5b
Sayali Sontakke <69626071+sayo24@users.noreply.github.com>
2020-09-11 15:53:20 +0530

    Feature and updates related to styling
    Feature and updates related to styling

---
+++ animate.js
@@ -0,0 +1,33 @@
+
+
+    document.getElementById("button2").addEventListener("click", function(){
+        // call back funciton of addevenetListener
+        console.log('inside eventListener function ');
+    document.getElementById("box").style.animationName="wobble";
+
+    });
+
+
+    
+    document.getElementById("button1").addEventListener("click", function(){
+        // call back funciton of addevenetListener
+        console.log('inside eventListener function ');
+    document.getElementById("box").style.animationName="flash";
+
+    });
+
+    
+    document.getElementById("button3").addEventListener("click", function(){
+        // call back funciton of addevenetListener
+        console.log('inside eventListener function ');
+    document.getElementById("box").style.animationName="heartBeat";
+
+    });
+
+    
+    document.getElementById("button4").addEventListener("click", function(){
+        // call back funciton of addevenetListener
+        console.log('inside eventListener function ');
+    document.getElementById("box").style.animationName="bounce";
+
+    });
